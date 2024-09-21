adaf56302c7e6b7c70656c4b604d23ed40142e63
APIVerve Team <hello@apiverve.com>
2024-09-21 01:19:03 -0500

    Bug Fixes

---
+++ PeriodicTableQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class PeriodicTableQueryOptions {
+public string name { get; set; }
+}
+
+}
